0d2a78231f4a016977e390674a8b821e59f59bfe
arahmawati606@gmail.com <ilar6780@gmail.com>
2023-06-05 00:34:05 +0700

    Update Button
    Intent sementara tombol masuk ke riwayat pelaporan

--- MassiveChallage/app/src/main/java/com/example/massivechallage/LoginActivity.kt
+++ MassiveChallage/app/src/main/java/com/example/massivechallage/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.massivechallage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    var btnSementara: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnSementara = findViewById(R.id.btnMasuk)
+
+        btnSementara?.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RiwayatLaporanActivity::class.java))
+        }
     }
 }
