bf77f1d21b43e6235e2523cae001fec0d6d737a5
arahmawati606@gmail.com <ilar6780@gmail.com>
2023-06-10 13:51:41 +0700

    Form Pelaporan

---
+++ MassiveChallage/app/src/main/java/com/example/massivechallage/ui/LaporanPolsekActivity.kt
@@ -0,0 +1,43 @@
+package com.example.massivechallage.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.example.massivechallage.R
+
+class LaporanPolsekActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laporan_polsek)
+
+        listView = findViewById(R.id.lv_pelaporan)
+        val polsek = arrayOf(
+            "Polsek Lubuk Baja", "polsek Batam Kota", "Polsek Sekupang",
+            "Polsek Nongsa", "Polsek Batu Aji", "Polsek Batu Ampar",
+            "Polsek Belakang Padang", "Polsek KKP", "Polsek Bengkong",
+            "Polsek Sei Beduk", "Polsek Sagulung", "Polsek Galang",
+            "Polsek KKB", "Polsek Barelang"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, polsek)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+
+            val polsek = polsek[position]
+
+            val i = Intent(this, FormPelaporanActivity::class.java)
+            i.putExtra("polsek", polsek)
+            startActivity(i)
+           /* val selectedItem = parent.getItemAtPosition(position) as String
+            Toast.makeText(this, "$selectedItem", Toast.LENGTH_SHORT).show()
+        */}
+
+    }
+}
