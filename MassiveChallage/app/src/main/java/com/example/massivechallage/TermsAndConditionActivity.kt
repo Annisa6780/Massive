5777d159dc07ec2a4f2ed0ae058577aa670394e8
Dianptrmhrn <dianputrim342@gmail.com>
2023-06-15 01:37:43 +0800

    update beberapa tampilan
    akun, edit akun, laporan, terms

---
+++ MassiveChallage/app/src/main/java/com/example/massivechallage/TermsAndConditionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.massivechallage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsAndConditionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_and_condition)
+    }
+}
