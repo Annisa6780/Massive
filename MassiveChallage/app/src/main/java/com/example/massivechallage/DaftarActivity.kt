3a2c1598213da7e86f46ce26ca7c0146f5fec294
arahmawati606@gmail.com <ilar6780@gmail.com>
2023-06-17 01:39:39 +0700

    Konfig Login + Regis
    Login + Regis sudah bisa menggunakan email dan password di fairebase, namum data pengguna belum terdata ke firebase

--- MassiveChallage/app/src/main/java/com/example/massivechallage/DaftarActivity.kt
+++ MassiveChallage/app/src/main/java/com/example/massivechallage/DaftarActivity.kt
@@ -1,15 +1,129 @@
 package com.example.massivechallage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
+import com.example.massivechallage.databinding.ActivityDaftarBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
+
+/*private var RadioGroup.error: String
+    get() {}
+    set() {}*/
 
 class DaftarActivity : AppCompatActivity() {
+
+    lateinit var  binding: ActivityDaftarBinding
+    lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityDaftarBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_daftar)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnDaftar.setOnClickListener {
+            val nama = binding.namaLengkap.text.toString()
+            val nik = binding.editTextNumber.text.toString()
+            val alamat = binding.alamatPelapor.text.toString()
+            val ttl = binding.editTextDate.text.toString()
+           /* val jenis_kelamin = binding.radioGroup.context.toString()*/ //perlu ditanyakan
+            val telp = binding.editTextPhone.text.toString()
+            val pekerjaan = binding.pekerjaan.text.toString()
+            //bagian upload surat pernyayaan blm
+            val email = binding.editTextTextEmailAddress.text.toString()
+            val kata_sandi = binding.editTextTextPassword2.text.toString()
+            val konfirm_pass = binding.editTextTextPassword3.text.toString()
+
+            if (nama.isEmpty()) {
+                binding.namaLengkap.error = "Nama Harus Terisi"
+                binding.namaLengkap.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (nik.isEmpty()) {
+                binding.editTextNumber.error = "Nomer Induk Kependudukan Harus Terisi"
+                binding.editTextNumber.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (alamat.isEmpty()) {
+                binding.alamatPelapor.error = "Alamat Harus Terisi"
+                binding.alamatPelapor.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (ttl.isEmpty()) {
+                binding.editTextDate.error = "Tanggal Lahir Harus Terisi"
+                binding.editTextDate.requestFocus()
+                return@setOnClickListener
+            }
+
+           /* if (jenis_kelamin.isEmpty()) {
+                binding.radioGroup.error = "Wajib Pilih Salah Satu"
+                binding.radioGroup.requestFocus()
+                return@setOnClickListener
+            }*/
+
+            if (telp.isEmpty()) {
+                binding.editTextPhone.error = "Nomor Telepon Harus Terisi"
+                binding.editTextPhone.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.PHONE.matcher(telp).matches()){
+                    binding.editTextPhone.error = "Nomor Telepon Tidak Valid"
+                    binding.editTextPhone.requestFocus()
+                    return@setOnClickListener
+            }
+
+            if (pekerjaan.isEmpty()) {
+                binding.pekerjaan.error = "Pekerjaan Harus Terisi"
+                binding.pekerjaan.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (email.isEmpty()) {
+                binding.editTextTextEmailAddress.error = "Email Harus Terisi"
+                binding.editTextTextEmailAddress.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.editTextTextEmailAddress.error = "Email Tidak Falid"
+                binding.editTextTextEmailAddress.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (kata_sandi.isEmpty()) {
+                binding.editTextTextPassword2.error = "Kata Sandi Harus Terisi"
+                binding.editTextTextPassword2.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (kata_sandi.length < 8) {
+                binding.editTextTextPassword2.error = "Kata Sandi Minimal Terdiri 8 Karakter"
+                binding.editTextTextPassword2.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (konfirm_pass.isEmpty()) {
+                binding.editTextTextEmailAddress.error = "Konfirmasi Kata Sandi Harus Terisi"
+                binding.editTextTextEmailAddress.requestFocus()
+                return@setOnClickListener
+            }
+            //kondisi jika konfirm pass tidak sama dgn pass?
+
+            DaftarFirebase(email,kata_sandi)
+        }
+
+        
 
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
 
@@ -24,4 +138,21 @@ class DaftarActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun DaftarFirebase(email: String, kataSandi: String) {
+
+        auth.createUserWithEmailAndPassword(email, kataSandi)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Pendaftaran Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
+
+
--- MassiveChallage/app/src/main/java/com/example/massivechallage/LoginActivity.kt
+++ MassiveChallage/app/src/main/java/com/example/massivechallage/LoginActivity.kt
@@ -4,19 +4,69 @@ import android.content.Intent
 import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.massivechallage.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    var btnLogin: Button? = null
-    var btnDaftar: TextView? = null
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth : FirebaseAuth
+
+    /*var btnLogin: Button? = null
+    var btnDaftar: TextView? = null*/
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
-        btnLogin = findViewById(R.id.btnMasuk)
+        auth = FirebaseAuth.getInstance()
+
+//setting tombol regis/daftar
+        binding.daftar.setOnClickListener {
+            val intent = Intent(this, DaftarActivity::class.java)
+            startActivity(intent)
+        }
+
+        //setting tombol login
+        binding.btnMasuk.setOnClickListener {
+            val email = binding.editTextTextPersonName.text.toString()
+            val kata_sandi = binding.editTextTextPassword.text.toString()
+
+            if (email.isEmpty()) {
+                binding.editTextTextPersonName.error = "Email Harus Terisi"
+                binding.editTextTextPersonName.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.editTextTextPersonName.error = "Email Tidak Falid"
+                binding.editTextTextPersonName.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (kata_sandi.isEmpty()) {
+                binding.editTextTextPassword.error = "Kata Sandi Harus Terisi"
+                binding.editTextTextPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (kata_sandi.length < 8) {
+                binding.editTextTextPassword.error = "Kata Sandi Minimal Terdiri 8 Karakter"
+                binding.editTextTextPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            LoginFirebase(email,kata_sandi)
+        }
+
+
+        /*btnLogin = findViewById(R.id.btnMasuk)
         btnLogin?.setOnClickListener {
             startActivity(Intent(this@LoginActivity, PelaporActivity::class.java))
         }
@@ -24,7 +74,21 @@ class LoginActivity : AppCompatActivity() {
         btnDaftar = findViewById(R.id.daftar)
         btnDaftar?.setOnClickListener {
             startActivity(Intent(this@LoginActivity, DaftarActivity::class.java))
-        }
+        }*/
 
+    }
+
+    private fun LoginFirebase(email: String, kataSandi: String) {
+
+        auth.signInWithEmailAndPassword(email, kataSandi)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Login Berhasil! Selamat Datang $email", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, BottomNavigationActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
