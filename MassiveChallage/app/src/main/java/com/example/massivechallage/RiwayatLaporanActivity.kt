b971e51a73315a0ef2250fd665dbc9d17872c022
Dianptrmhrn <dianputrim342@gmail.com>
2023-06-22 01:35:46 +0800

    perubahan pada riwayat laporan dan bottom navigation
    - mengubah tampilan halaman riwayat laporan yang awalnya menggunakan recycler view
    - menghapus bagian bottom navigation (bagian UI, bottom navigation activity, bottom navigation.xml, fragment home; laporan; kontak; edukasi, bottom nav menu, mobile navigation, kontak laporan fragment

--- MassiveChallage/app/src/main/java/com/example/massivechallage/RiwayatLaporanActivity.kt
+++ MassiveChallage/app/src/main/java/com/example/massivechallage/RiwayatLaporanActivity.kt
@@ -11,42 +11,42 @@ class RiwayatLaporanActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_riwayat_laporan)
 
-        val RiwayatLaporanList = listOf<DataRiwayatLaporan>(
-            DataRiwayatLaporan(
-                jenisLaporan = "Pelecehan Seksual",
-                tanggalLapor = "4 Juni 2023",
-                alamatPelapor = "Jl A",
-                statusLaporan = R.string.status_menunggu,
-                statuswarLaporan = R.color.status_menunggu
-            ),
-            DataRiwayatLaporan(
-                jenisLaporan = "Cyberbullying",
-                tanggalLapor = "6 Juni 2023",
-                alamatPelapor = "Jl b",
-                statusLaporan = R.string.status_proses,
-                statuswarLaporan = R.color.status_proses
-            ),
-            DataRiwayatLaporan(
-                jenisLaporan = "Eksploitasi",
-                tanggalLapor = "7 Juni 2023",
-                alamatPelapor = "Jl c",
-                statusLaporan = R.string.status_diterima,
-                statuswarLaporan = R.color.status_terima
-            ),
-            DataRiwayatLaporan(
-                jenisLaporan = "Eksploitasi",
-                tanggalLapor = "7 Juni 2023",
-                alamatPelapor = "Jl c",
-                statusLaporan = R.string.status_diterima,
-                statuswarLaporan = R.color.status_terima
-            )
-        )
-
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_riwayatlaporan)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = AdapterRiwayatLaporan(this, RiwayatLaporanList){
-
-        }
+//        val RiwayatLaporanList = listOf<DataRiwayatLaporan>(
+//            DataRiwayatLaporan(
+//                jenisLaporan = "Pelecehan Seksual",
+//                tanggalLapor = "4 Juni 2023",
+//                alamatPelapor = "Jl A",
+//                statusLaporan = R.string.status_menunggu,
+//                statuswarLaporan = R.color.status_menunggu
+//            ),
+//            DataRiwayatLaporan(
+//                jenisLaporan = "Cyberbullying",
+//                tanggalLapor = "6 Juni 2023",
+//                alamatPelapor = "Jl b",
+//                statusLaporan = R.string.status_proses,
+//                statuswarLaporan = R.color.status_proses
+//            ),
+//            DataRiwayatLaporan(
+//                jenisLaporan = "Eksploitasi",
+//                tanggalLapor = "7 Juni 2023",
+//                alamatPelapor = "Jl c",
+//                statusLaporan = R.string.status_diterima,
+//                statuswarLaporan = R.color.status_terima
+//            ),
+//            DataRiwayatLaporan(
+//                jenisLaporan = "Eksploitasi",
+//                tanggalLapor = "7 Juni 2023",
+//                alamatPelapor = "Jl c",
+//                statusLaporan = R.string.status_diterima,
+//                statuswarLaporan = R.color.status_terima
+//            )
+//        )
+//
+//        val recyclerView = findViewById<RecyclerView>(R.id.rv_riwayatlaporan)
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.setHasFixedSize(true)
+//        recyclerView.adapter = AdapterRiwayatLaporan(this, RiwayatLaporanList){
+//
+//        }
     }
 }
